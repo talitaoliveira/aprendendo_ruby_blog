c9c24293ac432ef52f39f47d96cb2ea49f087976
Talita Silva de Oliveira <litaa.oliveira@gmail.com>
2015-11-24 22:34:11 -0300

    criando model com db, feito o rake db:migrate, criando views

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151124155652) do
+ActiveRecord::Schema.define(version: 20151125010337) do
+
+  create_table "articles", force: :cascade do |t|
+    t.string   "title"
+    t.text     "text"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "produtos", force: :cascade do |t|
     t.string   "nome"
